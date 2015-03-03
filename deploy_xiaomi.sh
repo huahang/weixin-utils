d73f414be3d18f706277670720f9b636ab8f2d1f
liuhuahang <liuhuahang@zerus.co>
2015-03-03 16:48:54 +0800

    update xiaomi deploy
    N/A
    
    Signed-off-by: liuhuahang <liuhuahang@zerus.co>

--- deploy_xiaomi.sh
+++ deploy_xiaomi.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn clean package source:jar javadoc:jar test install deploy -DaltDeploymentRepository=archiva.snapshots::default::http://maven.n.miliao.com:8081/nexus/content/repositories/snapshots
+mvn clean package source:jar javadoc:jar test install deploy -DaltDeploymentRepository=archiva.snapshots::default::http://nexus.d.xiaomi.net:8081/nexus/content/repositories/snapshots
