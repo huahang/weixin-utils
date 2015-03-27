e3bf89dbe303422c5a918a3cd66a5612f8d84aca
liuhuahang <liuhuahang@zerus.co>
2015-03-27 16:46:32 +0800

    add weixin cryptography api
    N/A
    
    Signed-off-by: liuhuahang <liuhuahang@zerus.co>

---
+++ weixin-utils/src/test/java/im/chic/weixin/mp/aes/WeixinMessageEncryptionTest.java
@@ -0,0 +1,19 @@
+package im.chic.weixin.mp.aes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+/**
+ * @author huahang
+ */
+@RunWith(JUnit4.class)
+public class WeixinMessageEncryptionTest {
+    @Test
+    public void test() throws AesException, ParserConfigurationException, IOException, SAXException {
+    }
+}
